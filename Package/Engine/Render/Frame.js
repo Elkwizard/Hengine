7f110c0e1f697c2ac1126417a664290638890deb
Elkwizard <catsheeplord@gmail.com>
2020-05-10 22:50:36 -0700

    compatability

--- Package/Engine/Render/Frame.js
+++ Package/Engine/Render/Frame.js
@@ -1,24 +1,24 @@
-try {
-	new OffscreenCanvas(0, 0);
-} catch (e) {
-	OffscreenCanvas = class {
-		constructor(width, height) {
-			this.width = width;
-			this.height = height;
-			this.id = "Fake";
-			this.canvas = document.createElement("canvas");
-		}
-		getContext() {
-			return this.canvas.getContext("2d");
-		}
-	}
-}
-class Frame {
+let Frame = class {
 	constructor(width, height) {
 		this.width = width;
 		this.height = height;
 		this.img = new OffscreenCanvas(width, height);
 		this.c = new Artist(this.img);
 		this.c.c.imageSmoothingEnabled = !g.preservePixelart;
+	}
+}
+try {
+	new OffscreenCanvas(0, 0);
+} catch (e) {
+	Frame = class {
+		constructor(width, height) {
+			this.width = width;
+			this.height = height;
+			this.img = document.createElement("canvas");
+			this.img.width = width;
+			this.img.height = height;
+			this.c = new Artist(this.img);
+			this.c.c.imageSmoothingEnabled = !g.preservePixelart;
+		}
 	}
 }
