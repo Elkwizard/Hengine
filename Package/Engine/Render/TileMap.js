12fc2fc2bf08681b523ec087a9c3442def0b2e66
Elkwizard <bananamanthefourth@gmail.com>
2023-08-09 18:50:29 -0700

    added implicit section boundaries for TileMaps

--- Package/Engine/Render/TileMap.js
+++ Package/Engine/Render/TileMap.js
@@ -2,15 +2,27 @@ class TileMap {
 	constructor(image, sections) {
 		this.image = image;
 		this.tiles = new Map();
-		const img = image.makeImage();
 		for (let i = 0; i < sections.length; i++) {
-			const { name, x, y, width, height } = sections[i];
-			const frame = new Frame(width, height, 1);
-			frame.renderer.c.drawImage(img, x, y, width, height, 0, 0, width, height);
-			this.tiles.set(name, frame);
+			const { name, area } = sections[i];
+			this.tiles.set(name, Frame.fromImageType(image, area));
 		}
 	}
 	getTile(tileType) {
 		return this.tiles.get(tileType);
+	}
+	static regular(image, tileWidth, tileHeight, names) {
+		let x = 0;
+		let y = 0;
+		const sections = [];
+		for (let i = 0; i < names.length; i++) {
+			sections.push({ name: names[i], area: new Rect(x, y, tileWidth, tileHeight) });
+			x += tileWidth;
+			if (x >= image.width) {
+				x = 0;
+				y += tileHeight;
+			}
+		}
+
+		return new TileMap(image, sections);
 	}
 }
