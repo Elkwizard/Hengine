9480c3c10abc8c16ad90cd36c15c8907f1777f35
Elkwizard <bananamanthefourth@gmail.com>
2023-12-17 22:42:24 -0800

    Revert "ported physics engine to C++ with emscripten and WASM"
    This reverts commit 96336fb6ab47e0d66a7f74f1c9f78405cb08017c.

--- Package/Engine/Math/Physics/compile.sh
+++
@@ -1,2 +0,0 @@
-emcc $1.cpp -o $1.wasm -s STANDALONE_WASM -s WARN_ON_UNDEFINED_SYMBOLS=0 --no-entry -O3
-node WASMtoJS.js $@
--- Package/Engine/Math/Physics/physics.cpp
+++
@@ -1,8 +0,0 @@
-#include "engine.cpp"
-#include "rigidbody.cpp"
-#include "colliders.cpp"
-#include "resolver.cpp"
-#include "matrix.cpp"
-#include "constraints.cpp"
-
-int setup() { return 0; }
