2213665944e724d7d87c4c27b256e5ecc463a9ef
Elkwizard <bananamanthefourth@gmail.com>
2023-12-22 16:32:34 -0800

    optimized

--- Package/Engine/Math/Physics/compile.sh
+++ Package/Engine/Math/Physics/compile.sh
@@ -1,2 +1,2 @@
-emcc $1.cpp -o $1.wasm -s STANDALONE_WASM -s WARN_ON_UNDEFINED_SYMBOLS=0 --no-entry -std=c++20 -O3 -g
+emcc $1.cpp -o $1.wasm -s STANDALONE_WASM -s WARN_ON_UNDEFINED_SYMBOLS=0 --no-entry -std=c++20 -O3
 node WASMtoJS.js $@
--- Package/Engine/Math/Physics/order.hpp
+++ Package/Engine/Math/Physics/order.hpp
@@ -8,20 +8,20 @@ class OrderGenerator {
 		uint64_t seed;
 		
 		uint64_t next(uint64_t length) {
-			// seed++;
-			// seed ^= seed << 7;
-			// seed ^= seed >> 9;
-			// seed ^= seed << 13;
-			// seed -= 192834821ull;
-			// seed ^= seed << 7;
-			// seed ^= seed >> 9;
-			// seed ^= seed << 13;
-			// return seed % length;
 			seed++;
-			double a = fmod(seed * 638835776.12849, 8.7890975);
-			double b = fmod(a * 256783945.4758903, 2.567890);
-			double r = fmod(abs(a * b * 382749.294873597), 1.0);
-			return floor(r * length);
+			seed ^= seed << 7;
+			seed ^= seed >> 9;
+			seed ^= seed << 13;
+			seed -= 192834821ull;
+			seed ^= seed << 7;
+			seed ^= seed >> 9;
+			seed ^= seed << 13;
+			return seed % length;
+			// seed++;
+			// double a = fmod(seed * 638835776.12849, 8.7890975);
+			// double b = fmod(a * 256783945.4758903, 2.567890);
+			// double r = fmod(abs(a * b * 382749.294873597), 1.0);
+			// return floor(r * length);
 		}
 
 	public:
